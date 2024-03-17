e54cf796d85ca94984113f63e27397bdf811639f
Christopher.Bradley <declarationcb@gmail.com>
2024-03-17 09:13:32 +0000

    visual updates for demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,29 @@ export default async function Page() {
     functionName: 'owner',
   })
 
+  const job: any = await publicClient.readContract({
+    // @ts-ignore
+    address: jobBoardDeployment[publicClient.chain.id],
+    abi: JobBoardContract.abi,
+    functionName: 'jobs',
+    args: [0],
+  })
+
+  console.log(job)
+
   return (
     <div className='p-6 space-y-4 text-xs'>
       {/* @ts-ignore */}
       <div>Job Board Owner: {owner}</div>
       <Contract name='owner' value={owner} action={writeContract} func={'transferOwnership'} abi={JobBoardContract.abi} address={jobBoardDeployment[publicClient.chain.id]} />
+      <div>
+        <div>Job ID: {String(job[0])}</div>
+        <div>Employer: {String(job[1])}</div>
+        <div>Description: {String(job[2])}</div>
+        <div>Fulfillment Reward: {String(job[3])}</div>
+        <div>Is Open: {String(job[4])}</div>
+        <div>Successfull Applicant: {String(job[5])}</div>
+      </div>
     </div>
   );
 }
