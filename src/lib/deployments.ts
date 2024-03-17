e2de9075fcee98b027d2558ba6dc32ebaf98ee2f
Christopher.Bradley <declarationcb@gmail.com>
2024-03-17 08:52:29 +0000

    updated deployment address

--- src/lib/deployments.ts
+++ src/lib/deployments.ts
@@ -9,7 +9,5 @@ if (process.env.NODE_ENV !== "development") {
 export const greeterDeployment: {
   [chainId: number]: string;
 } = {
-  84532: !isDev
-    ? "0xF046f3e0a7eB061645d4e5062806767Ed13dca4D"
-    : GreeterContract.address,
+  84532: "0xF046f3e0a7eB061645d4e5062806767Ed13dca4D",
 };
